c35200af8e8d5adac9a271ffdf4d5c5cec75e8be
revail <valrevail@gmail.com>
2014-07-29 03:44:37 +0100

    Teste password
    teste password

--- js/code.js
+++ js/code.js
@@ -1,4 +1,3 @@
-alert("hi");
 var DB; // Base de dados
 
 function show(){
@@ -21,5 +20,65 @@ function initDB(){
 		var displayName = "Quotations";
 		var maxSize = 3*1024*1024; // 3MB
 		db = window.openDatabase(name,version,displayName,maxSize);
+
+		initTables();
+		initValues();
 	}
 }
+
+function initTables(){
+	db.transaction(function(tx){
+		tx.executeSql('CREATE TABLE IF NOT EXISTS BookAuthors (author_id INT NOT NULL, isbn TEXT NOT NULL , PRIMARY KEY (author_id,isbn));');
+	});
+	db.transaction(function(tx){
+		tx.executeSql('CREATe TABLE If not exists Authors (id int primary key, name text);');
+	});
+	db.transaction(function(tx){
+		tx.executeSql('CREATe TABLE If not exists Books (isbn primary key, title text, price real);');
+	});
+	db.transaction(function(tx){
+		tx.executeSql('CREATe TABLE If not exists Quotations (nom int primary key, isbn text, page int, quotation text);');
+	});
+}
+
+function initValues(){
+	db.transaction(function(tx){
+		tx.executeSql("INSERT INTO Authors (id,name) VALUES (5,'Uolter');");
+	});
+	db.transaction(function(tx){
+		tx.executeSql("INSERT INTO Books (isbn,title,price) VALUES ('4654654','Teste title','546');");
+	});
+	db.transaction(function(tx){
+		tx.executeSql("INSERT INTO BookAuthors (author_id,isbn) values (5,'4654654');");
+	});
+	db.transaction(function(tx){
+		tx.executeSql("INSERT INTO Quotations (nom,isbn,page,quotation) values (1,'4654654','45','vvvvvvvvvvv');");
+	});
+}
+
+function showQuatations(){
+	document.getElementById("menu").style.display = "none";
+	document.getElementById("quotations").style.display = "block";
+	document.getElementById("quotations").innerHTML = "";
+
+	db.transaction(function(tx){
+		tx.executeSql("select * from Authors ",[],function(tx,result){
+			var len = result.rows.length; // tamanho do array
+			var name;
+			for (var i = 0;i<len;i++){
+				name = result.rows.item(i).name;
+				alert(name);
+			}
+		},null);
+	});
+}
+
+function adicionarOutroAutor(){
+	db.transaction(function(tx){
+		tx.executeSql("INSERT INTO Authors (id,name) VALUES (2,'Uolter2');");
+	});
+}
+
+// CREATe TABLE If not exists Authors (id int primary key, name text);
+// INSERT INTO Authors (id,name) VALUES (5,'Test');
+// 6.57 parte 4
