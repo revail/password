a4b3a6a3d28f922b8ef0b6ec176f21d85fef4f8c
revail <valrevail@gmail.com>
2014-07-29 02:08:11 +0100

    Password
    password

---
+++ js/code.js
@@ -0,0 +1,24 @@
+var DB; // Base de dados
+
+function show(){
+	document.getElementById("intro").style.display = "none";
+	initDB();
+	
+}
+
+function initDB(){
+	// Web storage initial
+	if(!window.openDatabase){ // Verificação se é nativo com HTML5 WebStorage
+		document.getElementById("menu").innerHTML+= = "Atentção";
+		document.getElementById("status").style.display = "block";
+		document.getElementById("intro").style.display = "none";
+		return;
+	}else{
+		document.getElementById("menu").style.display = "block";
+		var name = "Quotations";
+		var version = "1.0";
+		var displayName = "Quotations";
+		var maxSize = 3*1024*1024 // 3MB
+		db = window.openDatabase(name,version,displayName,maxSize);
+	}
+}
